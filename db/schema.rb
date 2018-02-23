63f1e039663db27f0a19774e2c006fc55cc36e2d
An Nuyen <annuyen@gmail.com>
2018-02-23 21:04:50 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.float "price"
     t.string "image_url"
     t.integer "size"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
